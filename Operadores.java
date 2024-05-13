25c3ae74efe776b96ff4b403f7f80659c7b13b1a
Cristopher Andrade <acristopher986@gmail.com>
2024-05-13 09:47:21 -0300

    adicionar projeto jaba basico - sintaxe

---
+++ Operadores.java
@@ -0,0 +1,9 @@
+
+public class Operadores {
+    public static void main(String[] org){
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        //exemplo
+        System.out.println(nomeCompleto);
+    }
+    
+}
